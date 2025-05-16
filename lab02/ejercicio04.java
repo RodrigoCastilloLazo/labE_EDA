0caad599f9523e4c0b0e41bacead652d68164406
RodrigoCastilloLazo <rcastillola@unsa.edu.pe>
2025-05-15 19:13:10 -0500

    ejercicio 4 terminado

---
+++ lab02/ejercicio04.java
@@ -0,0 +1,29 @@
+package lab02;
+import java.util.Scanner;
+
+public class ejercicio04 {
+
+    public static void trianguloRecursivo2(int base) {
+        trianguloRecursivo2(base, base);
+    }
+    
+    private static void trianguloRecursivo2(int original, int actual) {
+        if (actual < 1) return;
+        trianguloRecursivo2(original, actual - 1);
+        for (int i = 0; i < original - actual; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < actual; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la base del triangulo: ");
+        int var = sc.nextInt();
+        trianguloRecursivo2(var);
+        sc.close();        
+    }
+}
